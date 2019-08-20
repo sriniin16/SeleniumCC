31a36c49f740e65398797525c9a322d395424824
sriniin16 <srini_in16@yahoo.com>
2019-08-20 08:16:25 +0530

    Initial commit

---
+++ Features/demo.feature
@@ -0,0 +1,4 @@
+Feature: Google practice
+  Scenario: Test selenium
+    Given I am on the google page
+
---
+++ src/test/java/Steps/AppTest.java
@@ -0,0 +1,24 @@
+package Steps;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("This is a test method");
+    }
+
+    @Test
+    public void MyTest1()
+    {
+        System.out.println("This is my test method");
+    }
+}
